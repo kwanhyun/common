488ca661d86c50a44d643159989b37eebbe5c0d1
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-07-23 23:50:58 +0900

    sp1 업데이트
    자료구조 중

--- ep1.py
+++ ep1.py
@@ -43,3 +43,33 @@ print('{0},{1},{2}'.format('a','b','c'))
 print('{},{},{}'.format('a','b','c'))
 print('{},{},{}'.format(*'abc')) #unpacking
 print('Coordinates: {lat},{lng}'.format(lat='37.24N',lng='-115.81W'))
+
+
+### 자료 구조
+numbers = [1,3,5,7]
+print(numbers)
+print(3 in numbers)
+print(10 in numbers)
+
+print(numbers[-3])
+
+print(len(numbers))
+
+numbers2 = [1,3,5,7,[1,2,3,3,3,3],8]
+print(len(numbers2))
+print(len(numbers2[4]))
+
+
+print('=============')
+for i in numbers2:
+    print(i)
+
+##데이터 변경
+numbers3 = [1, 3, 5, 7, 5]
+numbers3[0] = 10 # 지정 색인의 값을 변경
+numbers3.append(9) # 끝에 값을 추가
+numbers3.pop(3) # 특정 색인값을 가져옴과 동시에 제거
+numbers3.remove(5) # 특정 값을 1회 제거
+numbers3.insert(1, 11) # 특정 위치에 값을 추가
+
+print (numbers3)
