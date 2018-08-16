cd337e8f5253791ccfd3fc8dd66f86597419a6eb
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-08-17 00:38:20 +0900

    view detail

--- Django/askdjango/dojo/views.py
+++ Django/askdjango/dojo/views.py
@@ -1,6 +1,7 @@
 #dojo/views.py
+import os
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
 #def mysum(request,x,y=0,z=0):
@@ -19,3 +20,31 @@ def mysum(request,numbers):
 def hello(request, name, age):
     return HttpResponse ('안녕하세요. {}. {} 살이네요'.format(name,age))
     
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    '''.format(name=name))
+
+
+def post_list2(request):
+    name = '공유'
+    return render(request,'dojo/post_list.html',{'name':name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message':'Hi Python&Django',
+        'itmes':['Python','AWS','AZURE','DJANGO'],
+    },json_dumps_params={'ensure_ascii':False})
+
+
+def excel_download(request):
+    filepath = 'D:\GitRepo\common\Django\gdplev.xlsx'
+    filename = os.path.basename(filepath)
+    with open(filepath,'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel') # default text/html..
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
