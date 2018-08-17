c0fd35ae2e7b134de9488b53259048f6f0edb791
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-08-18 01:10:30 +0900

    Model 정의 및 마이그레이션

--- Django/askdjango/blog/models.py
+++ Django/askdjango/blog/models.py
@@ -1,5 +1,12 @@
 #blog/models.py
+import re
 from django.db import models
+from django.forms import ValidationError
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 
 class Post(models.Model):
@@ -11,6 +18,8 @@ class Post(models.Model):
 #    ))#길이 제한이 있는 문자열
     title = models.CharField(max_length=100, help_text='제목 입력..100내')#, verbose_name='제목')
     content = models.TextField() # 길이 제한이 없는 문자열
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=100, blank=True,validators=[lnglat_validator],help_text='경도/위도 포맷으로 입력')
     created_at = models.DateTimeField(auto_now_add=True) #최초 저장될 때 시간
     update_at = models.DateTimeField(auto_now=True) # 해당 레코드가 갱신이 될때 마다 일시 저장
 # Create your models here.
