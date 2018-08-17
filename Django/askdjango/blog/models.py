dcf2cb29654a315c2caf24185f8e6fa2c94164ff
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-08-18 00:53:25 +0900

    blog/models.post 모델 생성/마이그레이션

--- Django/askdjango/blog/models.py
+++ Django/askdjango/blog/models.py
@@ -1,3 +1,16 @@
+#blog/models.py
 from django.db import models
 
+
+class Post(models.Model):
+#    title = models.CharField(max_length=100,
+#    choices=(
+#        ('제목1','제목 1레이블'), #('저장될 값','UI에 보여질 레이블')
+#        ('제목2','제목 2레이블'),
+#        ('제목3','제목 3레이블'),
+#    ))#길이 제한이 있는 문자열
+    title = models.CharField(max_length=100, help_text='제목 입력..100내')#, verbose_name='제목')
+    content = models.TextField() # 길이 제한이 없는 문자열
+    created_at = models.DateTimeField(auto_now_add=True) #최초 저장될 때 시간
+    update_at = models.DateTimeField(auto_now=True) # 해당 레코드가 갱신이 될때 마다 일시 저장
 # Create your models here.
