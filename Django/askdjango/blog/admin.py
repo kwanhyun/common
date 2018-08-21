f6270a28085d9b09984ad365d8f95ff1b2b0b4fe
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-08-21 22:42:50 +0900

    admin 심화

--- Django/askdjango/blog/admin.py
+++ Django/askdjango/blog/admin.py
@@ -2,6 +2,28 @@
 from django.contrib import admin
 from .models import Post
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','status','title','content_size','created_at','update_at']
 
-admin.site.register(Post)
+    actions = ['make_draft','make_publishd']
+    
+
+    def content_size(self, post):
+       return '{}글자'.format(len(post.content))
+    content_size.short_description = '글자수'
+
+    def make_draft(self,request,queryset):
+        updated_cont = queryset.update(status='d')
+        self.message_user(request,'{}건의 포스팅을 Draft 상태로 변경'.format(updated_cont))
+
+    make_draft.short_description = '지정 포스팅을 Draft상태로 변경합니다.'
+
+    def make_publishd(self,request,queryset):
+        updated_cont = queryset.update(status='p')
+        self.message_user(request,'{}건의 포스팅을 Publshed 상태로 변경'.format(updated_cont))
+
+    make_publishd.short_description = '지정 포스팅을 Published상태로 변경합니다.'
+    
+#admin.site.register(Post, PostAdmin)
 # Register your models here.
--- Django/askdjango/blog/models.py
+++ Django/askdjango/blog/models.py
@@ -16,10 +16,16 @@ class Post(models.Model):
 #        ('제목2','제목 2레이블'),
 #        ('제목3','제목 3레이블'),
 #    ))#길이 제한이 있는 문자열
+    STATUS_CHOICES = (
+        ('d','Draft'),
+        ('p','Published'),
+        ('w','Withdrawn'),
+    )
     title = models.CharField(max_length=100, help_text='제목 입력..100내')#, verbose_name='제목')
     content = models.TextField() # 길이 제한이 없는 문자열
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=100, blank=True,validators=[lnglat_validator],help_text='경도/위도 포맷으로 입력')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True) #최초 저장될 때 시간
     update_at = models.DateTimeField(auto_now=True) # 해당 레코드가 갱신이 될때 마다 일시 저장
 # Create your models here.
