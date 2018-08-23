a0e940c47337874c8863e8f22c4c4d1000c60b19
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-08-24 02:06:21 +0900

    crawling Test

--- crawling/nano.py
+++ crawling/nano.py
@@ -1,15 +1,33 @@
 import requests
+import mechanicalsoup
 from bs4 import BeautifulSoup
 
-with requests.Session() as s:
-    get_parmas = {'username':'ncckh','password':'3wndThvmxm!'}
-    response = s.post('https://nano.ncsoft.com',data=get_parmas)
+browser = mechanicalsoup.StatefulBrowser()
 
-    print(response.status_code)
+browser.open('https://mlogin.plaync.com/login/signin') # request.get
+browser.select_form('form#loginForm')
+browser['login_name'] = 'ckh6666'
+browser['password'] = 'cdh1063744!'
+browser.submit_selected()
+#print(res.status_code)
 
-    post_pg = s.get('https://nano.ncsoft.com/notice/view?no=441839')
-    htmlMain = post_pg.text
+browser.launch_browser()
 
-    soup = BeautifulSoup(htmlMain,'html.parser')
-    print(soup.text)
+#response = requests.get('https://github.com/settings/emails')
+#html = response.text
+#print(html)
 
+
+   #get_parmas = {'username':'kwanhyun','password':'sky019293'}
+   #response = s.post('https://github.com/login',data=get_parmas)
+   #
+   #redirect_cookie = response.headers['Set-Cookie']
+   #headers = {'referer':'https://github.com/session'}
+   ##print(redirect_cookie)
+   ##print(response.status_code)
+
+   #post_pg = s.get('https://github.com/settings/emails',headers=headers)
+   #htmlMain = post_pg.text
+   #print(htmlMain)
+   #soup = BeautifulSoup(htmlMain,'html.parser')
+   #print(soup.text)
