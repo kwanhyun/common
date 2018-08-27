b0138e8d74ea2ef06004606ef6d50dc762694adb
drcho <ckh6666@naver.com>
2018-08-27 17:12:29 +0900

    nano crawling

--- crawling/crawling_test.py
+++ crawling/crawling_test.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 LOGIN_INFO = {
-    'login_name' : 'ckh6666',
-    'password': 'cdh1063744!'
+    'username' : 'ncckh',
+    'password': '3wndThvmxm!'
 }
 
 with requests.session() as s:
@@ -16,16 +16,40 @@ with requests.session() as s:
 
     #LOGIN_INFO = {**LOGIN_INFO, **{'app_id':auth['value']}}
     #print(LOGIN_INFO)
-
-    login_req = s.post('https://mlogin.plaync.com/login/signin',data=LOGIN_INFO)
+    '''plaync 성공
+    login_req = s.post('https://nano.ncsoft.com/signin',data=LOGIN_INFO)
     print(login_req.status_code)
     #headers = login_req.headers['Set-Cookie']
     headers = {
-        'referer':'https://mlogin.plaync.com/'
+        'referer':'https://nano.ncsoft.com/'
     }
 
-    response = s.get('https://id.plaync.com/account/main/intro',headers=headers)
+    response = s.get('https://nano.ncsoft.com/',headers=headers)
     html = response.text
     print(html)
+    '''
+
+    #nano 시도
+
+    first_page = s.get('https://nano.ncsoft.com/signin')
+    html = first_page.text
+    soup = bs(html,'html.parser')
+    csrf = soup.find('input',{'name':'_csrf'})
+
+    print(csrf['value'])
+
+    LOGIN_INFO = {**LOGIN_INFO,**{'_csrf':csrf['value']}}
+    print(LOGIN_INFO)
 
     
+    login_req = s.post('https://nano.ncsoft.com/signin',data=LOGIN_INFO)
+    print(login_req.status_code)
+    
+    
+    headers = {
+        'referer':'https://nano.ncsoft.com/'
+    }
+
+    response = s.get('https://nano.ncsoft.com/',headers=headers)
+    html = response.text
+    print(html)
