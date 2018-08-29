18f6b23c7eebe8b002e086f1c86648f4cf1f8ee6
drcho <ckh6666@naver.com>
2018-08-29 16:01:25 +0900

    SendEmail Test Commit

--- automation.py
+++ automation.py
@@ -1,1 +1,23 @@
 #Email
+import smtplib
+from email.message import EmailMessage
+
+import getpass
+password = getpass.getpass('Password : ')
+
+message = EmailMessage()
+message['Subject'] = '이메일 제목'
+message['From'] = 'ckh6666@naver.com'
+message['To'] = 'ckh6666@hanmail.net'
+message.set_content(
+'''
+테스트 Email 입니다.
+'''
+)
+
+with smtplib.SMTP_SSL('smtp.naver.com',465) as server:
+    server.ehlo()
+    server.login('ckh6666',password)
+    server.send_message(message)
+
+print('Send Email..')
--- crawling/crawling_test.py
+++ crawling/crawling_test.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 LOGIN_INFO = {
-    'login_name' : 'ckh6666',
-    'password': 'cdh1063744!'
+    'username' : 'ncckh',
+    'password': '5smfskfTl!'
 }
 
 with requests.session() as s:
@@ -15,21 +15,21 @@ with requests.session() as s:
     #print(auth['value'])
     #LOGIN_INFO = {**LOGIN_INFO, **{'app_id':auth['value']}}
     #print(LOGIN_INFO)
-
-    login_req = s.post('https://mlogin.plaync.com/login/signin',data=LOGIN_INFO)
+    '''
+    login_req = s.post('https://nano.ncsoft.com/signin',data=LOGIN_INFO)
     print(login_req.status_code)
     #headers = login_req.headers['Set-Cookie']
     #headers = {
     #    'referer':'https://nano.ncsoft.com/'
     #}
 
-    response = s.get('https://id.plaync.com/account/main/intro')#,headers=headers)
+    response = s.get('https://nano.ncsoft.com/')#,headers=headers)
     html = response.text
     print(html)
     
+    '''
 
     #nano 성공
-    '''
 
     first_page = s.get('https://nano.ncsoft.com/signin')
     html = first_page.text
@@ -50,12 +50,12 @@ with requests.session() as s:
         'referer':'https://nano.ncsoft.com/'
     }
 
-    response = s.get('https://nano.ncsoft.com/',headers=headers)
+    response = s.get('https://nano.ncsoft.com/notice/view?no=442255',headers=headers)
     html = response.text
     print(html)
 
 #데스크시도
-    
+'''   
     login_req = s.post('https://servicedesk.korea.ncsoft.corp',data=LOGIN_INFO,verify=False)
     print(login_req.status_code)
     
