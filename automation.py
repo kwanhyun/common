47bfe82ae22a7db328bb8271a084e20cbd61f001
drcho <ckh6666@naver.com>
2018-08-30 13:11:00 +0900

    메일 보내기 이미지 첨부해서 보내기

--- automation.py
+++ automation.py
@@ -1,6 +1,8 @@
 #Email
 import smtplib
 from email.message import EmailMessage
+from email.mime.application import MIMEApplication
+import os
 
 import getpass
 password = getpass.getpass('Password : ')
@@ -18,11 +20,25 @@ message.set_content(
 message.add_alternative('''
     <h1>AskDjango VOD</h1>
 
+    <img src="cid:test.jpg" />
+
     <ul>
         <li>test test</li>
     </ul>
 
 ''',subtype='html')
+
+#filepath = './test.jpg'
+#이미지 첨부
+
+filepath_list = ['./test.jpg','./test1.jpg','./test2.jpg']
+for filepath in filepath_list:
+    with open(filepath,'rb') as f:
+        filename = os.path.basename(filepath)
+        img_date = f.read()
+        part = MIMEApplication(img_date,name=filename)
+        message.attach(part)
+
 
 with smtplib.SMTP_SSL('smtp.naver.com',465) as server:
     server.ehlo()
