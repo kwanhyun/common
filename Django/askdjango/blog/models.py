cbdb622056f1a3f0588d0e3f6c645acade8d6d3e
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-08-31 23:01:13 +0900

    template 상속
    2단계 상속 전까지 학습

--- Django/askdjango/blog/models.py
+++ Django/askdjango/blog/models.py
@@ -2,6 +2,7 @@
 import re
 from django.db import models
 from django.forms import ValidationError
+from django.conf import settings
 
 
 def lnglat_validator(value):
@@ -21,6 +22,7 @@ class Post(models.Model):
         ('p','Published'),
         ('w','Withdrawn'),
     )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100, help_text='제목 입력..100내')#, verbose_name='제목')
     content = models.TextField() # 길이 제한이 없는 문자열
     tags = models.CharField(max_length=100, blank=True)
