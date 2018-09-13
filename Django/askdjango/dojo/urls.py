0e655f6dbad2ba99594cedb344a69b6e862a87d5
kwanhyun <41185299+kwanhyun@users.noreply.github.com>
2018-09-14 00:24:06 +0900

    Django Form

--- Django/askdjango/dojo/urls.py
+++ Django/askdjango/dojo/urls.py
@@ -3,6 +3,7 @@ from . import views
 from . import views_cbv
 
 urlpatterns =[
+    url(r'^new/$',views.post_new),
     #url(r'^sum/(?P<x>\d+)$',views.mysum),
     #url(r'^sum/(?P<x>\d+)/(?P<y>\d+)$',views.mysum),
     #url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)$',views.mysum),
--- Django/askdjango/dojo/views.py
+++ Django/askdjango/dojo/views.py
@@ -1,12 +1,48 @@
 #dojo/views.py
 import os
-from django.shortcuts import render
+from django.shortcuts import redirect,render
 from django.http import HttpResponse, JsonResponse
+from .forms import PostForm
+from .models import Post
 # Create your views here.
 
 #def mysum(request,x,y=0,z=0):
 #    #request: HttpRequset <<- 함수 정의 할 때 무조건 선언한다.
 #    return HttpResponse(int(x)+100+int(y)+int(z))
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            #방법1)
+            #post = Post()
+            #post.title= form.cleaned_data['title']
+            #post.Content= form.cleaned_data['content']
+            #post.save()
+            #return redirect('/dojo/')
+            
+            #방법2)
+            #post = Post(title= form.cleaned_data['title'],
+            #Content= form.cleaned_data['content'])
+            #post.save()
+            #return redirect('/dojo/')
+            
+            #방법3)
+            #post = Post.objects.create(title= form.cleaned_data['title'],
+            #Content= form.cleaned_data['content'])
+            
+            #방법4)
+            form.cleaned_data
+            post = Post.objects.create(**form.cleaned_data) #DB에 저장하기
+            post.save()
+            return redirect('/success.url/')
+        else:
+            form.errors
+    else:
+        form = PostForm()
+    return render(request,'dojo/post_form.html',{
+        'form': form
+    })
 
 def mysum(request,numbers):
     #request: HttpRequset <<- 함수 정의 할 때 무조건 선언한다.
